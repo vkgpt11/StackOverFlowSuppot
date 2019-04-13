247c88a8da3e1006afe472c9521764a84bba98f8
vikas-kr-gupta <vikasgupta.net@live.com>
2019-04-13 22:25:19 +0530

    initial code

---
+++ MixedDesktopApps/WpfApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (e.Args.Length > 0)
+            {
+                MessageBox.Show($"You have passed:{e.Args.Length} arguments," +
+                    $" value are {string.Join( ",",e.Args)}");
+            }
+        }
+    }
+}

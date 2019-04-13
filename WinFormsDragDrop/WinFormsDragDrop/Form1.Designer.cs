b78f213943627e8c682a41f37a33bf5db1f45e82
vikas-kr-gupta <vikasgupta.net@live.com>
2019-04-14 00:05:02 +0530

    - initial drag drop code

---
+++ WinFormsDragDrop/WinFormsDragDrop/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WinFormsDragDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Location = new System.Drawing.Point(26, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(264, 242);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView1_ItemDrag);
+            // 
+            // listView2
+            // 
+            this.listView2.AllowDrop = true;
+            this.listView2.Location = new System.Drawing.Point(365, 82);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(264, 242);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView2_DragDrop);
+            this.listView2.DragOver += new System.Windows.Forms.DragEventHandler(this.listView2_DragOver);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 437);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ WinFormsDragDrop/WinFormsDragDrop/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsDragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.AllowDrop = true;
+            listView2.AllowDrop = true;
+            listView1.Items.Add(new ListViewItem() { Text="List 1 Item 1" });
+            listView1.Items.Add(new ListViewItem() { Text = "List 1 Item 2" });
+            listView2.Items.Add(new ListViewItem() { Text = "List 2 Item 1" });
+            listView2.Items.Add(new ListViewItem() { Text = "List 2 Item 2" });
+
+        }
+
+
+
+
+        private void listView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            var items = new List<ListViewItem>();
+            items.Add((ListViewItem)e.Item);
+            foreach (ListViewItem lvi in listView1.SelectedItems)
+            {
+                if (!items.Contains(lvi))
+                {
+                    items.Add(lvi);
+                }
+            }
+            listView1.DoDragDrop(items, DragDropEffects.Move);
+        }
+
+        private void listView2_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(List<ListViewItem>)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void listView2_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(List<ListViewItem>)))
+            {
+                var items = (List<ListViewItem>)e.Data.GetData(typeof(List<ListViewItem>));
+                foreach (ListViewItem lvi in items)
+                {
+                    listView1.Items.Remove(lvi);
+                    listView2.Items.Add(lvi);
+                }
+            }
+        }
+
+    }
+}
